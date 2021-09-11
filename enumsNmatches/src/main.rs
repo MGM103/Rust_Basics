17e02e72f8add47341bddc715cc820448f213cdb
MGM103 <nelli@DESKTOP-KSRFUIU.localdomain>
2021-09-11 12:36:46 +1000

    Adding completed enums exercise

---
+++ enumsNmatches/src/main.rs
@@ -0,0 +1,11 @@
+fn main() {
+    //enum types can have different types and different number of types
+    enum IpAddr {
+        V4(u8, u8, u8, u8),
+        V6(String),
+    }
+
+    let home = IpAddr::V4(127, 0, 0, 1);
+
+    let loopback = IpAddr::V6(String::from("::1"));
+}
